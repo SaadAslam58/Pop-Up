3150aeffed9885e3e1cad3d6c2e9e33397cbd4c3
SaadAslam58 <saadxpert58@gmail.com>
2024-10-20 23:43:49 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+let popup = document.querySelector('#popup');
+
+function onclick(){
+    popup.classList.add("open-popup")
+}
+function openPopup(){
+    popup.classList.add("open-popup")
+}
+function closePopup(){
+    popup.classList.remove("open-popup")
+}
